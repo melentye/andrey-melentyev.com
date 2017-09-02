280e063a5790c85a5b499a15d5a2d075f6aeab56
Andrey Melentyev <andrey.melentyev@gmail.com>
2017-09-02 10:57:27 +0100

    Use https for pelican

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://andrey-melentyev.com'
+SITEURL = 'https://www.andrey-melentyev.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
