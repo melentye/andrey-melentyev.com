355646da56ff729fd5edbc17680c927814ae8406
Andrey Melentyev <andrey.melentyev@gmail.com>
2017-10-22 18:15:08 +0200

    Keybase verification

--- pelicanconf.py
+++ pelicanconf.py
@@ -71,6 +71,7 @@ STATIC_PATHS = [
 # Control over the target location of the pages
 EXTRA_PATH_METADATA = {
     'static/yandex_44baf762e45a6938.html': {'path': 'yandex_44baf762e45a6938.html'},
+    'static/keybase.txt': {'path': 'keybase.txt'},
     'static/robots.txt': {'path': 'robots.txt'},
     'static/resume.html': {'path': 'resume.html'},
     'static/resume.css': {'path': 'resume.css'},
